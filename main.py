3fd9b441956ce11243836f9efe3abd1cbe29385e
Ashish David <58704058+AshishDavid@users.noreply.github.com>
2023-02-23 23:24:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ from openpyxl import load_workbook
 import math
 import argparse
 from scipy.optimize import fsolve
+import json
+import csv
+
+# defining constants
+pfeed = 1.01
+qsweep = 50
+lmembrane = 0.1
 
 
-def func(x):
+def calc_avg_bg():
+    count = 0
+    sum_bg_n = 0
+    for k in range(2, sheet.max_row + 1):
+        if sheet["H" + str(k)].value == "switch time":
+            break
+        count += 1
+    for k in range(count - 30, count + 1):
+        sum_bg_n = sum_bg_n + int(sheet["D" + str(k)].value)
+    avg_bg_n = sum_bg_n / 30
+    return avg_bg_n
+
+
+def calc_rsi(x):
     return 355 * math.log10(
-        (1.01 * (io / x) / ((io / x) + pmsar)) / (1.01 * pmsar / ((io / x) + pmsar)) * 100) - 32 - x
+        (pfeed * (io / x) / ((io / x) + pmsar)) / (pfeed * pmsar / ((io / x) + pmsar)) * 100) - 32 - x
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=argparse.FileType('r'))
+parser.add_argument('fileformat')
+parser.add_argument('rso')
+parser.add_argument('dmembrane')
 args = parser.parse_args()
 workbook = load_workbook(filename=args.filename.name)
 sheet = workbook.active
 
-sum_bg = 0
-count = 0
-for i in range(2, sheet.max_row + 1):
-    if sheet["H" + str(i)].value == "switch time":
-        break
-    count += 1
-for i in range(count - 30, count + 1):
-    sum_bg = sum_bg + int(sheet["D" + str(i)].value)
-avg_bg = sum_bg / 30
+avg_bg = calc_avg_bg()
 print("BG:   ", end='')
 print(avg_bg)
 for i in range(1, sheet.max_row + 1):
@@ -55,7 +70,7 @@ pmsar = sum_ / c
 print(pmsar)
 
 print("rs-0: ", end='')
-print(126.9)
+print(args.rso)
 
 real = h2_raw - avg_bg
 print("Real: ", end='')
@@ -66,7 +81,7 @@ print("I-0: ", end='')
 print(io)
 
 print("rs-i: ", end='')
-rsi = fsolve(func, 1)
+rsi = fsolve(calc_rsi, 1)
 print(rsi[0])
 
 pmsi = io / rsi[0]
@@ -89,7 +104,6 @@ qpiqar = ppi / ppar * 100
 print("Q-p,i/Q-Ar,i: ", end='')
 print(qpiqar)
 
-qsweep = 50
 print("Q-Sweep(Ar): ", end='')
 print(qsweep, end='')
 print(" mln/min")
@@ -99,16 +113,14 @@ print("Q-permeate: ", end='')
 print(qpermeate, end='')
 print(" mln/min")
 
-pfeed = 1.01
-dmembrane = 2.7
-amembrane = 3.141593 / 4 * dmembrane * dmembrane
+amembrane = 3.141593 / 4 * float(args.dmembrane) * float(args.dmembrane)
 
 print("P-feed: ", end='')
 print(pfeed, end='')
 print(" bar")
 
 print("d-membrane: ", end='')
-print(dmembrane, end='')
+print(args.dmembrane, end='')
 print(" cm")
 
 print("A-membrane: ", end='')
@@ -124,7 +136,6 @@ print("Permeance in GPU: ", end='')
 print(permeancegpu)
 
 permeancepa = permeancegpu * 3.35 / 1000
-lmembrane = 0.1
 permeability = permeancegpu * lmembrane
 
 print("Permeance in 10^-7 mol/(m^2 s Pa): ", end='')
@@ -135,3 +146,23 @@ print(lmembrane)
 
 print("Permeability in Barrer: ", end='')
 print(permeability)
+data = {"BG": avg_bg, "Raw": h2_raw, "p-ms-ar": pmsar, "rs-0": args.rso, "Real": real, "I-O": io,
+        "rs-i"
+        : str(rsi[0]), "P-MS-I": pmsi, "P-MS-Tot": pmstot, "P-p,i": ppi, "P-p,Ar,i": ppar, "Q-p,i/Q-Ar,i": qpiqar,
+        "Q-Sweep(Ar)":
+            qsweep, "Q-permeate": qpermeate, "P-feed": pfeed, "d-membrane": args.dmembrane, "A-membrane": amembrane,
+        "Permeance in bar": permeancebar, "Permeance in GPU": permeancegpu, "Permeance in Pa": permeancepa,
+        "L-membrane": lmembrane, "Permeability in Barrer": permeability}
+if args.fileformat == "json":
+    with open("output_in_json.json", "w") as outfile:
+        json.dump(data, outfile)
+elif args.fileformat == "csv":
+    fieldnames = ['BG', 'Raw', 'p-ms-ar', 'rs-0', 'Real', 'I-0', 'rs-i', 'P-MS-I', 'P-MS-Tot', 'P-p,i', 'P-p,Ar,i',
+                  'Q-p,i/Q-Ar,i',
+                  'Q-Sweep(Ar)', 'Q-permeate', 'P-feed', 'd-membrane', 'A-membrane', 'Permeance in bar',
+                  'Permeance in GPU',
+                  'Permeance in Pa', 'Permeability in Barrer', 'L-membrane', 'I-O']
+    with open("output_in_csv.csv", 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows([data])
